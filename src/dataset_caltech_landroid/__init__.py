8e21c7553d4286df39dd579dbb6860e8af9e0c74
Andrea Censi <andrea@cds.caltech.edu>
2014-08-14 10:52:18 -0400

    minor

--- src/dataset_caltech_landroid/__init__.py
+++ src/dataset_caltech_landroid/__init__.py
@@ -5,8 +5,5 @@ def jobs_comptests(context):
     GlobalConfig.global_load_dirs(['dataset_caltech_landroid.configs'])
 
     # Our tests are its tests with our configuration
-    from rawlogs import unittests
-
-    from comptests import jobs_registrar
-    from rawlogs import get_rawlogs_config
-    jobs_registrar(context, get_rawlogs_config())
+    from rawlogs import jobs_comptests
+    jobs_comptests(context)
